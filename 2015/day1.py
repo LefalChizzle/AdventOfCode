939599efaf480fda4d98fdadcd392c92a44d02f0
Chaithanya Gowda <chaig03@gmail.com>
2020-12-15 14:53:16 +0000

    2015 day1

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,4 +1,13 @@
 with open('files/day1.txt') as file:
-    brackets = file.readlines()
+    select = {
+        "(": 1,
+        ")": -1
+    }
+    brackets = [select.get(i) for i in file.read()]
 
-print(brackets)
+print(f"part1: {sum(brackets)}")
+
+for i in range(len(brackets)):
+    if sum(brackets[:i]) == -1:
+        print(f"part2: {i}")
+        break
